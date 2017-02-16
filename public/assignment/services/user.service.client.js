f1b30b31003935154d3f851d17390e30cea59c13
kmcarol26 <kmcarol26@ccis.neu.edu>
2017-02-15 23:54:49 -0500

    'commit2'

--- public/assignment/services/user.service.client.js
+++ public/assignment/services/user.service.client.js
@@ -7,10 +7,10 @@
 
     function userService() {
         var users = [
-            {_id: "123", username: "alice", password: "alice", firstName: "Alice", lastName: "Wonder"},
-            {_id: "234", username: "bob", password: "bob", firstName: "Bob", lastName: "Marley"},
-            {_id: "345", username: "charly", password: "charly", firstName: "Charly", lastName: "Garcia"},
-            {_id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose", lastName: "Annunzi"}
+            {_id: "123", username: "alice", password: "alice", firstName: "Alice", lastName: "Wonder",email:"alice@gmail.com"},
+            {_id: "234", username: "bob", password: "bob", firstName: "Bob", lastName: "Marley",email:"alice@gmail.com"},
+            {_id: "345", username: "charly", password: "charly", firstName: "Charly", lastName: "Garcia",email:"alice@gmail.com"},
+            {_id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose", lastName: "Annunzi",email:"alice@gmail.com"}
         ];
 
         var api = {

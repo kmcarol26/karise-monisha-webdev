aa84a505ac818dc6e7c03a8f50f2ce329b0e4cdf
kmcarol26 <kmcarol26@ccis.neu.edu>
2017-03-22 14:52:30 -0400

    'application'

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-var connectionString = 'mongodb://127.0.0.1:27017/test';
-var mongoose = require("mongoose");
-mongoose.connect(connectionString);
-
+//var connectionString = 'mongodb://127.0.0.1:27017/test';
+//var mongoose = require("mongoose");
+//mongoose.connect(connectionString);
+/*
 if(process.env.MLAB_USERNAME) {
   connectionString = process.env.MLAB_USERNAME + ":" +
   process.env.MLAB_PASSWORD + "@" +
   process.env.MLAB_HOST + ':' +
   process.env.MLAB_PORT + '/' +
   process.env.MLAB_APP_NAME;
-}
+}*/
 var express = require('express');
 var app = express();
 
@@ -18,9 +18,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // configure a public directory to host static content
 app.use(express.static(__dirname + '/public'));
-
-//require ("./test/app.js")(app);
 require("./assignment/app.js")(app);
+require ("./test/app.js")(app);
+
 
 var port = process.env.PORT || 3000;
 
